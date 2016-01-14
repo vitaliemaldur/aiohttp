4f221e15b4d44927e64097d124755d7794e67670
Andrew Svetlov <andrew.svetlov@gmail.com>
2016-01-14 19:57:29 +0200

    Sync with upstream

--- tests/test_urldispatch.py
+++ tests/test_urldispatch.py
@@ -62,8 +62,7 @@ class TestUrlDispatcher(unittest.TestCase):
     def test_register_route(self):
         handler = self.make_handler()
         route = PlainRoute('GET', handler, 'test', '/handler/to/path')
-        with self.assertWarns(DeprecationWarning):
-            self.router.register_route(route)
+        self.router.register_route(route)
 
         req = self.make_request('GET', '/handler/to/path')
         info = self.loop.run_until_complete(self.router.resolve(req))
